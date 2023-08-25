6ad9b5999ebd19baa4e001bf27b3c7bdd5082334
KatiFerrari <katielidasilvaferrari26@gmail.com>
2023-08-25 14:17:09 -0400

    commit inicial

---
+++ src/Todo/ToDo.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function ToDo() {
+   const [atividade, setAtividade ] = useState("");
+   const [lista, setLista ] = useState([]);
+   
+    const salvar =(e) =>{
+        e.preventDefault();
+        setLista([...lista, {
+                atividade: atividade
+        }]);
+        console.log(lista)
+    };
+
+    return (
+        <div>
+            <Link to="/">home</Link>
+            <h1>Lista de Atividades</h1>    
+            <form onSubmit={salvar}>
+            <input type="text"
+            onChange={(e)=>{ setAtividade(e.target.value)}}/>
+            <button>ADD</button>   
+            </form>      
+            {lista.map((ativ)=>
+            <div>
+                <p>ativ.atividade</p>
+            </div>
+            )} 
+        </div>
+    );
+}
